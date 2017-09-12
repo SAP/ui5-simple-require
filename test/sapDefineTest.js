fdadcfd239ec0ddd4a6d056e1f83eb69f9cac95e
Frederico Bittencourt <fred.rbittencourt@gmail.com>
2017-09-12 18:18:39 -0300

    add tests

---
+++ test/sapDefineTest.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let expect = require('chai').expect;
+let SAPDefine = require('../src/sapDefine');
+
+context("Test SAP define global override", function() {
+	
+	describe("Override sap global", function() {
+		it("Should add .uou to sap variable", function() {
+			SAPDefine.importFactory("/test/UI5ModuleExample", { uou: "pourra" } );
+			expect(sap.uou).to.be.equal("pourra");
+		});
+	});
+
+}); 
