3c99625ffd943377d9ce6522ff8f09449574a8b1
Frederico Bittencourt <fred.rbittencourt@gmail.com>
2018-09-17 17:57:14 -0300

    fix globalContextIssue test

--- test/globalContextIssueTest.js
+++ test/globalContextIssueTest.js
@@ -9,21 +9,12 @@ const ui5require = API.ui5require;
 context("", function() {
 
   it("Import with global context", function(done) {
-    ui5require('/test/example/UI5ModuleExample')
-      .onResolve((m) => {
-        expect(m).to.be.an("object");
-        done();
-      });
-
-/*
     ui5require("/test/example/UI5GlobalSAPExample")
       .globalSAP({ value: "abc" })
       .onResolve((m) => {
-        console.debug(`Module is ${m}`);
         expect(m.value).to.be.equal("abc");
         done();
       });
-      */
   });
 
   it("Import another global context", function(done) {
